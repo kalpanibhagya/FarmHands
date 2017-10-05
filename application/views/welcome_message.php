bb8252aa63210e296a8b830f5a600e32cc1fb0a6
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-05 23:17:52 +0530

    Add nav bar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -2,134 +2,52 @@
 <html lang="en">
 <head>
     <title>Organic Farming</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
-
-    <link rel="stylesheet" type="text/css" href="org.css">
 </head>
 <body>
-<div class="container-fluid" id="header">
-    <div class="container">
 
-
-        <div class="col-md-8">
-            <div class="row">
-                <div class="col-md-1">
-                    <a href="#" class="lin"></a>
-                </div>
-                <div class="col-md-3">
-                    <a href="#" class="lin">Farm Hands</a>
-                </div>
-                <div class="col-md-2">
-                    <a href="#" class="lin">Blog</a>
-                </div>
-                <div class="col-md-2">
-                    <a href="#" class="lin">Farms</a>
-                </div>
-                <div class="col-md-2">
-                    <a href="#" class="lin">About Us</a>
-                </div>
-                <div class="col-md-2">
-                    <a href="#" class="lin"></a>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="row">
-                <div class="col-md-3">
-                    <a href="#" class="lin"></a>
-                </div>
-                <div class="col-md-4">
-                    <a href="#" class="lin">Sign Up</a>
-                </div>
-                <div class="col-md-4">
-                    <a href="#" class="lin">Log In</a>
-                </div>
-                <div class="col-md-1">
-                    <a href="#" class="lin"></a>
-                </div>
-            </div>
-        </div>
-
-
-
-
-    </div>
-
-</div>
-<br>
-
-<div class="container-fluid">
-
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-
-        <div class="carousel-inner">
-            <div class="item active">
-                <img src="2.jpg" class="imgs" >
-                <div class="carousel-caption">
-
-                    <h2 style="color: white;margin-bottom: 150px;font-family: font-family: 'Rubik', sans-serif;">Organic Farming Volunteering Community Sri Lanka</h2>
-
-                </div>
+<header>
+    <nav class="navbar navbar-default navbar navbar-inverse">
+        <div>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Farm_Hands</a>
             </div>
 
-            <div class="item">
-                <img src="Is-Organic-Farming-the-Future-2.jpg" class="imgs" >
-                <div class="carousel-caption">
-
-                    <h2 style="color: white;margin-bottom: 100px;font-family: font-family: 'Rubik', sans-serif;">Organic Farming Volunteering Community Sri Lanka</h2>
-
-                </div>
-            </div>
-
-            <div class="item">
-                <img src="o_farmerStory_370.jpg"  class="imgs">
-                <div class="carousel-caption">
-
-                    <h2 style="color: white;margin-bottom: 100px;font-family: font-family: 'Rubik', sans-serif;">Organic Farming Volunteering Community Sri Lanka</h2>
-                </div>
-            </div>
-
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
-</div>
-
-
-
-
-<div class="container-fluid" id="prelast">
-    <div class="container">
-        <a href="#"><button type="button" id="butn" class="btn btn-success">Get Started</button></a>
-    </div>
-
-
-
-
-</div>
-
-
-
-
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Link</a></li>
+                </ul>
+                <form class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
 </body>
 </html>
