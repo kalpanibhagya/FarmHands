fbdc3455efdb3f165b950eb9763ab090d4696f8f
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-05 23:02:47 +0530

    Modify welcome_message.php file

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,135 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <title>Organic Farming</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-	<style type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+    <link rel="stylesheet" type="text/css" href="org.css">
 </head>
 <body>
+<div class="container-fluid" id="header">
+    <div class="container">
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+        <div class="col-md-8">
+            <div class="row">
+                <div class="col-md-1">
+                    <a href="#" class="lin"></a>
+                </div>
+                <div class="col-md-3">
+                    <a href="#" class="lin">Farm Hands</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="#" class="lin">Blog</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="#" class="lin">Farms</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="#" class="lin">About Us</a>
+                </div>
+                <div class="col-md-2">
+                    <a href="#" class="lin"></a>
+                </div>
+            </div>
+        </div>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col-md-3">
+                    <a href="#" class="lin"></a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="lin">Sign Up</a>
+                </div>
+                <div class="col-md-4">
+                    <a href="#" class="lin">Log In</a>
+                </div>
+                <div class="col-md-1">
+                    <a href="#" class="lin"></a>
+                </div>
+            </div>
+        </div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+
+    </div>
+
 </div>
+<br>
+
+<div class="container-fluid">
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="2.jpg" class="imgs" >
+                <div class="carousel-caption">
+
+                    <h2 style="color: white;margin-bottom: 150px;font-family: font-family: 'Rubik', sans-serif;">Organic Farming Volunteering Community Sri Lanka</h2>
+
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="Is-Organic-Farming-the-Future-2.jpg" class="imgs" >
+                <div class="carousel-caption">
+
+                    <h2 style="color: white;margin-bottom: 100px;font-family: font-family: 'Rubik', sans-serif;">Organic Farming Volunteering Community Sri Lanka</h2>
+
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="o_farmerStory_370.jpg"  class="imgs">
+                <div class="carousel-caption">
+
+                    <h2 style="color: white;margin-bottom: 100px;font-family: font-family: 'Rubik', sans-serif;">Organic Farming Volunteering Community Sri Lanka</h2>
+                </div>
+            </div>
+
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+</div>
+
+
+
+
+<div class="container-fluid" id="prelast">
+    <div class="container">
+        <a href="#"><button type="button" id="butn" class="btn btn-success">Get Started</button></a>
+    </div>
+
+
+
+
+</div>
+
+
+
 
 </body>
 </html>
