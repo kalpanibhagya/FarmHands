7c495755309f2ab8768b1e14eb523c8c1d595121
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-07 05:16:39 +0530

    Add images

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -6,48 +6,76 @@
 </head>
 <body>
 
-<header>
     <nav class="navbar navbar-default navbar navbar-inverse">
         <div>
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
                 <a class="navbar-brand" href="#">Farm_Hands</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Link</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Farms</a></li>
+                    <li><a href="#">About us</a></li>
                 </ul>
-                <form class="navbar-form navbar-left">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Search">
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Link</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li role="separator" class="divider"></li>
-                            <li><a href="#">Separated link</a></li>
-                        </ul>
-                    </li>
+                    <li><a href="#">Sign up</a></li>
+                    <li><a href="#">Login</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
-</header>
+<section>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel" data-intervel="2000">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img class="first-slide" src="<?php echo base_url(); ?>assets/images/2.jpg" alt="First slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 style="font-size: 400%">Farm_Hands</h1>
+                        <p style="font-size: 200%">Organic Farming Volunteering Community Sri Lanka</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Get Started</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img class="second-slide" src="<?php echo base_url(); ?>assets/images/3.jpg" alt="Second slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 style="font-size: 400%">Farm_Hands</h1>
+                        <p style="font-size: 200%">Organic Farming Volunteering Community Sri Lanka</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Get Started</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="item">
+                <img class="third-slide" src="<?php echo base_url(); ?>assets/images/4.jpg" alt="Third slide">
+                <div class="container">
+                    <div class="carousel-caption">
+                        <h1 style="font-size: 400%">Farm_Hands</h1>
+                        <p style="font-size: 200%">Organic Farming Volunteering Community Sri Lanka</p>
+                        <p><a class="btn btn-lg btn-primary" href="#" role="button">Get Started</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div><!-- /.carousel -->
+</section>
 </body>
 </html>
