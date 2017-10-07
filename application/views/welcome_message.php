39c6cb0705bdf27f5ca07755d764ac1200ba9fd0
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-07 05:51:20 +0530

    Add navbar

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -18,6 +18,27 @@
     <![endif]-->
 </head>
 <body>
+<nav class="navbar navbar-default navbar navbar-inverse">
+    <div>
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">Farm_Hands</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Farms</a></li>
+                <li><a href="#">About us</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Sign up</a></li>
+                <li><a href="#">Login</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
