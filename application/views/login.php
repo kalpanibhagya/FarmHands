bf84bcf8e53f9bc0d6c81f0d39ae038832960846
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-08 11:50:20 +0530

    Add background image to the login page

--- application/views/login.php
+++ application/views/login.php
@@ -12,7 +12,7 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 </head>
-<body>
+<body style="background-image: url('<?php echo base_url(); ?>assets/images/background.jpg')">
 <nav class="navbar navbar-default navbar navbar-inverse">
     <div>
         <div class="navbar-header">
