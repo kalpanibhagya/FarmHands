295a65b41c1b9db7a385f5dfe9051de7e0c74fa3
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-08 15:46:12 +0530

    Add a panel to login

--- application/views/login.php
+++ application/views/login.php
@@ -31,34 +31,47 @@
         </div>
     </div>
 </nav>
-<div class="container">
-    <h3 align="center">Login</h3>
-    <form action="<?php echo base_url()?>main/login_validation" method="post">
-        <div class="form-group">
-            <label>Username</label>
-            <input type="text" class="form-control" name="username">
-            <span class="text-danger"><?php echo form_error('username')?></span>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title text-center">Login</h3>
+                    </div>
+                    <div class="panel-body">
+                        <form action="<?php echo base_url()?>main/login_validation" method="post">
+                            <?php
+                            echo $this->session->flashdata('error');
+                            ?>
+                            <div class="form-group">
+                                <label>Username</label>
+                                <input type="text" class="form-control" name="username">
+                                <span class="text-danger"><?php echo form_error('username')?></span>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <input type="password" class="form-control" name="password">
+                                <span class="text-danger"><?php echo form_error('password')?></span>
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" class="btn btn-info" name="Submit" value="Submit">
+                                <br/>
+                                <a href="forget.php">Forget password?</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label>Password</label>
-            <input type="password" class="form-control" name="password">
-            <span class="text-danger"><?php echo form_error('password')?></span>
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btn btn-info" name="Submit" value="Submit">
-            <?php
-             echo $this->session->flashdata('error');
-            ?>
-        </div>
-    </form>
-</div>
-
-<div class="container">
-    <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-    </footer>
-</div>
+    </div>
+    <div class="container">
+        <footer>
+            <p class="pull-right"><a href="#">Back to top</a></p>
+            <p>&copy; 2016 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+    </div>
+</section>
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
