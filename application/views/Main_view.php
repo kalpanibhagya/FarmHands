44d745d4ef5924f677d01ae74c8f180b35126ac1
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-08 23:09:34 +0530

    Add more fields to sign up page

--- application/views/Main_view.php
+++ application/views/Main_view.php
@@ -16,8 +16,8 @@
 <body style="background-image: url('<?php echo base_url(); ?>assets/images/background.jpg')">
 
 <header>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -26,21 +26,21 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" style="font-size: xx-large;font-weight: bolder" href="<?php echo base_url(); ?>" >Farm_Hands</a>
+                <a class="navbar-brand" style="font-size: x-large;font-weight: bolder" href="<?php echo base_url(); ?>" >Farm_Hands</a>
             </div>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <form class="navbar-form navbar-right" action="<?php echo base_url()?>main/login_validation" method="post">
-                    <div class="form-group"><input type="text" style="height: 30px" class="form-control" name="username" placeholder="Username"></div>
-                    <div class="form-group"><input type="password" style="height: 30px" class="form-control" name="password" placeholder="Password"></div>
-                    <input type="submit" class="btn btn-primary" name="Login" value="Login" style="height: 30px"><br />
+                    <div class="form-group"><input type="text" class="form-control" name="username" placeholder="Username"></div>
+                    <div class="form-group"><input type="password" class="form-control" name="password" placeholder="Password"></div>
+                    <input type="submit" class="btn btn-primary" name="Login" value="Login">
                     <a href="#">Forgot password?</a><?php echo $this->session->flashdata('error'); ?><span class="text-danger"><?php echo form_error('username')?></span><span class="text-danger"><?php echo form_error('password')?></span>
                 </form>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
+            </div>
+        </div>
 </header>
-<section>
+<section style="margin-top: 100px">
     <div class="container">
         <div class="row">
             <div class="col-md-4 col-md-offset-4">
@@ -70,8 +70,33 @@
                                 <input type="password" class="form-control" name="confirm_password">
                                 <span class="text-danger"><?php echo form_error('confirm_password')?></span>
                             </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input type="email" class="form-control" name="confirm_password">
+                                <span class="text-danger"><?php echo form_error('email')?></span>
+                            </div>
+                            <div class="form-group">
+                                <label>First Name</label>
+                                <input type="text" class="form-control" name="fname">
+                                <span class="text-danger"><?php echo form_error('first_name')?></span>
+                            </div>
+                            <div class="form-group">
+                                <label>Last Name</label>
+                                <input type="email" class="form-control" name="lname">
+                                <span class="text-danger"><?php echo form_error('last_name')?></span>
+                            </div>
+                            <div class="form-group">
+                                <label>Address</label>
+                                <input type="text" class="form-control" name="address">
+                                <span class="text-danger"><?php echo form_error('address')?></span>
+                            </div>
+                            <div class="form-group">
+                                <label>Telephone</label>
+                                <input type="text" class="form-control" name="telephone">
+                                <span class="text-danger"><?php echo form_error('telephone')?></span>
+                            </div>
                             <div class="form-group text-center">
-                                <input type="submit" class="btn btn-info" name="Submit" value="Submit">
+                                <input type="submit" class="btn btn-info" name="Sign up" value="Sign up">
                                 <br/>
                                 <a href="<?php echo base_url(); ?>main/login">Already have an account?</a>
                             </div>
