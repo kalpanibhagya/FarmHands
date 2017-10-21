b66295958493c5f9c6d610c81b72ac6ee6976f51
kalpanibhagya <kalpanibhagya.kb@gmail.com>
2017-10-21 18:32:45 +0530

    Modify contact details in the footer

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -250,9 +250,9 @@
         <div class="row">
             <div class="col-sm-5">
                 <p>Contact us and we'll get back to you within 24 hours.</p>
-                <p><span class="glyphicon glyphicon-map-marker"></span> Chicago, US</p>
-                <p><span class="glyphicon glyphicon-phone"></span> +00 1515151515</p>
-                <p><span class="glyphicon glyphicon-envelope"></span> myemail@something.com</p>
+                <p><span class="glyphicon glyphicon-map-marker"></span> Colombo, Sri Lanka</p>
+                <p><span class="glyphicon glyphicon-phone"></span> 011 1515151</p>
+                <p><span class="glyphicon glyphicon-envelope"></span> farmhands@gmail.com</p>
             </div>
             <div class="col-sm-7 slideanim">
                 <div class="row">
@@ -266,7 +266,7 @@
                 <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
                 <div class="row">
                     <div class="col-sm-12 form-group">
-                        <button class="btn btn-default pull-right" type="submit">Send</button>
+                        <button class="btn btn-primary pull-right" type="submit">Send</button>
                     </div>
                 </div>
             </div>
