638f4ed7410d4f0cd7274d518c65de742b36166c
Nethma Thathsarani <95thathsarani@gmail.com>
2017-12-16 22:17:55 +0530

    blog redesigned

---
+++ application/views/blog/include/bloghead.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Organic Farming</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="Quickly Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ 
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet">
+   <script src="http://cdn.ckeditor.com/4.7.3/standard/ckeditor.js"></script>
+
+ <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="assets/css/style1.css" rel="stylesheet" type="text/css" media="all" />
+<link href="assets/css/style5.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="assets/js/move-top.js"></script>
+<script type="text/javascript" src="assets/js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+
+</head>
+<body>
+<!-- banner-body -->
+	<div class="banner-body">
+		<div class="container">
+<!-- header -->
+			<div class="header">
+				<div class="header-nav">
+					<nav class="navbar navbar-default">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+						  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						  </button>
+						   <a class="navbar-brand" href="index.html"><span>F</span>arm Hands</a>
+						</div>
+
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
+						 <ul class="nav navbar-nav">
+							<li class="hvr-bounce-to-bottom active"><a href="<?php echo base_url();?>Blog">Blog</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="about.html">About</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="<?php echo base_url();?>Blog/posts/create">Create Post</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="<?php echo base_url(); ?>Blog/categories/create">Create Category</a></li>
+							<li class="hvr-bounce-to-bottom"><a href="<?php echo base_url(); ?>Blog/recent">Recent Posts</a></li>
+						  </ul>
+						  <div class="sign-in">
+							<ul>
+								<li><a href="login.html">Sign In </a>/</li>
+								<li><a href="register.html">Register</a></li>
+							</ul>
+							</div>
+						</div><!-- /.navbar-collapse -->
+					</nav>
+				</div>
+				<!-- search-scripts -->
+			<script src="assets/js/classie.js"></script>
+			<script src="assets/js/uisearch.js"></script>
+				<script>
+					new UISearch( document.getElementById( 'sb-search' ) );
+				</script>
+			<!-- //search-scripts -->
+			</div>
+<!-- //header -->
+
+		
+				
+			
+				
+				
+
+				
+						
